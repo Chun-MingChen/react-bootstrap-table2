1b74fc9ecd4613fcae224504fe56fff9268c40bf
Chun-MingChen <nick830314@gmail.com>
2018-03-27 00:28:18 +0800

    example temp

--- packages/react-bootstrap-table2-example/examples/column-filter/text-filter.js
+++ packages/react-bootstrap-table2-example/examples/column-filter/text-filter.js
@@ -6,19 +6,6 @@ import { productsGenerator } from 'utils/common';
 
 const products = productsGenerator(8);
 
-const columns = [{
-  dataField: 'id',
-  text: 'Product ID'
-}, {
-  dataField: 'name',
-  text: 'Product Name',
-  filter: textFilter()
-}, {
-  dataField: 'price',
-  text: 'Product Price',
-  filter: textFilter()
-}];
-
 const sourceCode = `\
 import BootstrapTable from 'react-bootstrap-table-next';
 import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
@@ -39,14 +26,52 @@ const columns = [{
 <BootstrapTable keyField='id' data={ products } columns={ columns } filter={ filterFactory() } />
 `;
 
-export default () => (
-  <div>
-    <BootstrapTable
-      keyField="id"
-      data={ products }
-      columns={ columns }
-      filter={ filterFactory() }
-    />
-    <Code>{ sourceCode }</Code>
-  </div>
-);
+export default class extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+      columns: [{
+        dataField: 'id',
+        text: 'Product ID'
+      }, {
+        dataField: 'name',
+        text: 'Product Name',
+        filter: textFilter({
+          getFilterBy: (filterBy) => {
+            this.filterBy = filterBy;
+          }
+        })
+      }, {
+        dataField: 'price',
+        text: 'Product Price',
+        filter: textFilter()
+      }]
+    };
+  }
+
+
+  handleClick() {
+    this.filterBy('0');
+  }
+
+  render() {
+    return (
+      <div>
+        <button className="btn btn-primary" onClick={ this.handleClick }>
+          click to filter product name by 0
+        </button>
+
+        <BootstrapTable
+          keyField="id"
+          data={ products }
+          columns={ this.state.columns }
+          filter={ filterFactory() }
+        />
+        <Code>{ sourceCode }</Code>
+      </div>
+    );
+  }
+}
+
